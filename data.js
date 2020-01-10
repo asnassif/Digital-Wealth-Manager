6db03df25ee58180812776efa88b969a30773094
Amgad <you@example.com>
2020-01-10 08:47:13 -0500

    added files

---
+++ data.js
@@ -0,0 +1,26 @@
+var data = [{
+    ticker_symbol:"AMZN" 
+    date: 2020-01-09,
+    time: 16:00:00,
+    Open: 1899.9100,
+    high: 1901.0500,
+    low: 1899.6900,
+    close: 1900.1400,
+    volume:85629,
+    Industry: "Catalog/Specialty Distribution"
+    Sector:"Consumer Services"
+  },
+  {
+    ticker_symbol:"AMZN" 
+    date: 2020-01-09,
+    time: 15:55:00,
+    Open: 1898.1400,
+    high: 1900.2900,
+    low: 1897.9600,
+    close: 1899.7200,
+    volume:51241,
+    Industry: "Catalog/Specialty Distribution"
+    Sector:"Consumer Services"
+  },
+  
+];
